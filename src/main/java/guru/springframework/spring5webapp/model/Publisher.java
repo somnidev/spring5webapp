826bcf3eb2d39274875d4288a634f9e2ac4f027a
Simon <sqeebs@gmail.com>
2018-02-22 17:37:28 +0100

    Adds equals/hashcode/toString to Publisher entity

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Publisher {
@@ -49,5 +50,27 @@ public class Publisher {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
